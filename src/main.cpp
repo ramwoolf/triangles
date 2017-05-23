9544c71148e3bd45bbf86d292344268793e60b07
ramwoolf <ramwoolf@gmail.com>
2017-05-23 14:58:22 +0700

    add trivial main and check definitions

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "/usr/include/tinyxml2.h"
+
+namespace Volkov_Test {
+
+	bool check_triangles_intersection(const double a[], const double b[]) {
+		return false;
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	using namespace std;
+	/* code */
+	return 0;
+}
